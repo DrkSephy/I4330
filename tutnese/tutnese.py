a39cc9b685dc0bc5c65f6940eab6648bd0cba3ee
David Leonard <sephirothcloud1025@yahoo.com>
2015-02-23 22:06:13 -0500

    First implementation of encoder

--- tutnese/tutnese.py
+++ tutnese/tutnese.py
@@ -1,9 +1,11 @@
 import re
 
 language = {
+	'a': 'a',
 	'b': 'bub',
 	'c': 'coch',
 	'd': 'dud',
+	'e': 'e',
 	'f': 'fuf',
 	'g': 'gug',
 	'h': 'hash',
@@ -12,11 +14,13 @@ language = {
 	'l': 'lul',
 	'm': 'mum',
 	'n': 'nun',
+	'o': 'o', 
 	'p': 'pup',
 	'q': 'quack',
 	'r': 'rur',
 	's': 'sus',
 	't': 'tut',
+	'u': 'u',
 	'v': 'vuv',
 	'w': 'wack',
 	'x': 'xux',
@@ -32,3 +36,13 @@ languageReversed = dict (zip(language.values(), language.keys()))
 
 # Get list of keys in reversed dictionary
 languageReversedKeys = languageReversed.keys()
+
+def encode(phrase):
+	final_str = ''
+	for char in phrase:
+		# print languageReversed[char]
+		pattern = re.compile(char)
+		final_str += re.sub(pattern, language[char], char)
+	return final_str
+
+print encode('hello')
