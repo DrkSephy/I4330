be8dacbbec10a741449ab8ea2dd5d2a7456e7502
David Leonard <sephirothcloud1025@yahoo.com>
2015-02-27 12:23:20 -0500

    Testing tutnese decoder

--- tutnese/tests.py
+++ tutnese/tests.py
@@ -6,18 +6,22 @@
 
 import unittest
 from tutnese import encode
+from tutnese import decode
+from tutnese import tutneseLexer
+from tutnese import tutneseLex
 
 class TestTutneseFunctions(unittest.TestCase):
 
 	def setUp(self):
 		self.string = 'Over hill, over dale, Thorough bush, thorough brier, Over park, over pale, Thorough flood, thorough fire!'
 		self.encode = 'ovuverur hashisqual, ovuverur dudalule, tuthashorurougughash bubusushash, tuthashorurougughash bubrurierur, ovuverur puparurkuck, ovuverur pupalule, tuthashorurougughash fufluloodud, tuthashorurougughash fufirure!'
+		self.decode = self.string.lower()
 
 	def testEncode(self):
-		print encode(self.string)
-		print self.encode
 		self.assertEqual(encode(self.string), self.encode)
 
+	def testDecode(self):
+		self.assertEqual(decode('ovuverur hashisqual, ovuverur dudalule, tuthashorurougughash bubusushash, tuthashorurougughash bubrurierur, ovuverur puparurkuck, ovuverur pupalule, tuthashorurougughash fufluloodud, tuthashorurougughash fufirure!'), 'over hill, over dale, thorough bush, thorough brier, over park, over pale, thorough flood, thorough fire!')
 
 if __name__ == '__main__':
 	unittest.main()
